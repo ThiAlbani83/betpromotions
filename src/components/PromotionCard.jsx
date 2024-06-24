cbc9782a5e557d0e0086639987cd7b4154a264a5
Thiago <thialbani02@gmail.com>
2024-06-24 10:56:08 -0300

    button degrade change

--- src/components/PromotionCard.jsx
+++ src/components/PromotionCard.jsx
@@ -14,7 +14,7 @@ export default function PromotionCard({ id, image, title, description }) {
             <img src={image} alt='promospaceman' className='w-[250px] h-[250px] object-fit' />
             <h4 className='text-lg tracking-wider font-medium text-betpink mt-2'>{title}</h4>
             <p className='text-sm mt-2 max-w-[250px]'>{description}</p>
-            <button onClick={handleClick} className='w-full text-center bg-betpink rounded-md py-3 mt-16 active:scale-90 duration-500'>+ Detalhes</button>
+            <button onClick={handleClick} className='w-full text-center bg-gradient-to-r from-betpurple to-betpink rounded-md py-3 mt-16 active:scale-90 duration-500'>+ Detalhes</button>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ import { images, data } from '../constants'
 
 const Home = () => {
     return (
-        <main className='flex flex-col gap-10'>
-            <MainBanner desktop={images.leomourabanner} mobile={images.leomourabannermobile} />
+        <main className='flex flex-col gap-10'>         
+                <MainBanner desktop={images.leomourabanner} mobile={images.leomourabannermobile} />
             <div className='w-full px-5 sm:px-10 lg:px-20 flex flex-col gap-20'>
                 <h4 className='max-w-4xl text-base leading-[40px]'>Fique por dentro das nossas <span className='uppercase leading-[40px] font-bold text-lg bg-gradient-to-r from-betpurple to-betpink rounded-full px-2 py-1'>promoções</span> e saia na frente. Novas <span className='uppercase leading-[40px] font-bold text-lg bg-gradient-to-r from-betpurple to-betpink rounded-full px-2 py-1'>oportunidades</span> toda semana somente para você. Vem com a gente!</h4>
                 <h3 className='text-2xl font-medium tracking-widest underline underline-offset-8'>Cassino</h3>
