a2bf46aa12a5a74eb9fc5f48d6a77f351be71709
Thiago <thialbani02@gmail.com>
2024-06-24 10:42:59 -0300

    Adjusts on links and font spacing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,7 @@ import logo from '../assets/logobetfive.png'
 function Navbar() {
     return (
         <nav className='w-full px-5 sm:px-10 lg:px-32 py-5 flex items-center justify-between'>
-            <img src={logo} alt='logobetfive' className='w-[125px] h-[85px]' />
-            <Link to={"/"} className='underline text-sm font-medium tracking-wide text-left w-full max-w-[100px]        sm:max-w-fit'>Termos e Condições
-            </Link>
+            <Link to={"/"}><img src={logo} alt='logobetfive' className='w-[125px] h-[85px]' /></Link>
         </nav>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <main className='flex flex-col gap-10'>
             <MainBanner desktop={images.leomourabanner} mobile={images.leomourabannermobile} />
             <div className='w-full px-5 sm:px-10 lg:px-20 flex flex-col gap-20'>
-                <h4 className='max-w-4xl text-base leading-loose'>Fique por dentro das nossas <span className='uppercase font-bold text-lg bg-gradient-to-r from-betpurple to-betpink rounded-full px-2 py-1'>promoções</span> e saia na frente. Novas <span className='uppercase font-bold text-lg bg-gradient-to-r from-betpurple to-betpink rounded-full px-2 py-1'>oportunidades</span> toda semana somente para você. Vem com a gente!</h4>
+                <h4 className='max-w-4xl text-base leading-[40px]'>Fique por dentro das nossas <span className='uppercase leading-[40px] font-bold text-lg bg-gradient-to-r from-betpurple to-betpink rounded-full px-2 py-1'>promoções</span> e saia na frente. Novas <span className='uppercase leading-[40px] font-bold text-lg bg-gradient-to-r from-betpurple to-betpink rounded-full px-2 py-1'>oportunidades</span> toda semana somente para você. Vem com a gente!</h4>
                 <h3 className='text-2xl font-medium tracking-widest underline underline-offset-8'>Cassino</h3>
             </div>
             <div className='w-full px-5 sm:px-10 lg:px-20 flex flex-wrap items-center justify-center lg:justify-start gap-5 sm:gap-10 md:gap-14'>
